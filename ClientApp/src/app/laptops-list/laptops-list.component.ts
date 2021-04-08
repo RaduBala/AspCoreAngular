1d85c86e5d753b2e1cc0f72830fa1916435f31d1
RaduBala <balauroiu.radu@yahoo.com>
2021-04-08 13:25:53 +0300

    test3

--- ClientApp/src/app/laptops-list/laptops-list.component.ts
+++ ClientApp/src/app/laptops-list/laptops-list.component.ts
@@ -1,4 +1,4 @@
-import { Laptop } from './../models/laptop';
+
 import { DeviceService } from '../services/device.service';
 import { Component, OnInit } from '@angular/core';
 
@@ -11,7 +11,7 @@ import { Component, OnInit } from '@angular/core';
 export class LaptopsListComponent implements OnInit 
 {
     laptops: any[] ;
-/***/
+
     constructor(private laptopService: DeviceService) 
     { 
 
